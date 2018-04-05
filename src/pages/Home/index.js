5dc18d3e9bc8aaa5a745fc8a77973cf09740e9c3
Higo Ribeiro <HORBJN@gmail.com>
2018-04-05 14:56:32 -0300

    Ajustado o para classe a Page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,45 +1,57 @@
-import React from 'react';
+/* Core */
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+/* Redux */
+import { connect } from 'react-redux';
+
+/* Presentational */
 import { View, TouchableOpacity } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 
 import IconMaterial from 'react-native-vector-icons/MaterialIcons';
 import IconAwesome from 'react-native-vector-icons/FontAwesome';
 
+/* Styles */
 import { colors } from 'styles';
-
 import styles from './styles';
 
-const Home = () => (
-  <View style={styles.container}>
-    <Calendar
-      theme={{
-        backgroundColor: colors.primaryDarker,
-        calendarBackground: colors.primaryDarker,
-        selectedDayBackgroundColor: colors.secondary,
-        selectedDayTextColor: colors.white,
-        textDisabledColor: colors.white,
-        monthTextColor: colors.white,
-        dayTextColor: colors.white,
-        arrowColor: colors.white,
-        todayTextColor: colors.white,
-      }}
-    />
-  </View>
-);
+class Home extends Component {
+  static propTypes = {
+  };
+  static navigationOptions = ({ navigation }) => ({
+    title: 'SCHEDULE',
+    headerTitleStyle: styles.headerTitle,
+    headerLeft: (
+      <TouchableOpacity onPress={() => {}} style={styles.headerLeft}>
+        <IconMaterial name="add-circle" size={24} color={colors.secondary} />
+      </TouchableOpacity>
+    ),
+    headerRight: (
+      <TouchableOpacity onPress={() => { navigation.navigate({ routeName: 'Profile' })}} style={styles.headerRight} >
+        <IconAwesome name="user" size={22} color={colors.white} />
+      </TouchableOpacity>
+    ),
+  });
+  render() {
+    return (
+      <View style={styles.container}>
+        <Calendar
+          theme={{
+            backgroundColor: colors.primaryDarker,
+            calendarBackground: colors.primaryDarker,
+            selectedDayBackgroundColor: colors.secondary,
+            selectedDayTextColor: colors.white,
+            textDisabledColor: colors.white,
+            monthTextColor: colors.white,
+            dayTextColor: colors.white,
+            arrowColor: colors.white,
+            todayTextColor: colors.white,
+          }}
+        />
+      </View>
+    );
+  }
+}
 
-Home.navigationOptions = {
-  title: 'SCHEDULE',
-  headerTitleStyle: styles.headerTitle,
-  headerLeft: (
-    <TouchableOpacity onPress={() => {}} style={styles.headerLeft}>
-      <IconMaterial name="add-circle" size={24} color={colors.secondary} />
-    </TouchableOpacity>
-  ),
-  headerRight: (
-    <TouchableOpacity onPress={() => {}} style={styles.headerRight} >
-      <IconAwesome name="user" size={22} color={colors.white} />
-    </TouchableOpacity>
-  ),
-};
-
-export default Home;
+export default connect()(Home);
