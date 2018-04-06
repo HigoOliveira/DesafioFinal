e4bc86c0b8e6ef426c6c2f68e2548f3fc28f7c86
Higo Ribeiro <HORBJN@gmail.com>
2018-04-06 10:56:54 -0300

    Adicionado novas cores

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,10 +1,13 @@
 export default {
   primary: '#A55BD7',
   secondary: '#49C7A7',
-
+  dustyGrey: '#999999',
   primaryDarker: '#8B48B8',
 
+  light: '#DDD',
+  gallery: '#EEEEEE',
   white: '#FFFFFF',
+
   red: '#E35E5E',
   blue: '#5ECCE3',
 };
