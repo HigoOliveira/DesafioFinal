392d4740dee8c1947a5f74bfe85a90e0300ea602
Higo Ribeiro <HORBJN@gmail.com>
2018-04-06 17:28:47 -0300

    Adicionado duck para os eventos

---
+++ src/store/ducks/event.js
@@ -0,0 +1,14 @@
+import { createReducer, createActions } from 'reduxsauce';
+
+/* Types & Action Creators */
+const { Types, Creators } = createActions({
+
+});
+
+export { Types };
+export default Creators;
+
+/* Initial State */
+export const INITIAL_STATE = {
+  list: [],
+};
