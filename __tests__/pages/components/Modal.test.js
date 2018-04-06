e0e583cf369a88d32c696ad7585784df8d2b494f
Higo Ribeiro <HORBJN@gmail.com>
2018-04-06 17:25:26 -0300

    Adicionado o Test do Model

---
+++ __tests__/pages/components/Modal.test.js
@@ -0,0 +1,90 @@
+/* Core */
+import React from 'react';
+
+/* Test */
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+/**
+ * Presentational
+ */
+import Modal from 'pages/Home/components/Modal';
+import Alert from 'components/Alert';
+import Button from 'components/Button';
+import Input from 'components/Input';
+import InputDatePicker from 'components/InputDatePicker';
+/* redux */
+import configureStore from 'redux-mock-store';
+
+const mockStore = configureStore([]);
+
+const initialStore = {
+  user: {
+  },
+};
+
+const name = 'Aprendendo GoNative';
+const where = 'Viçosa, Minas Gerais';
+const datetime = new Date();
+
+describe('Modal', () => {
+  const store = mockStore(initialStore);
+  let wrapper;
+  let alert;
+
+  function createWrapper() {
+    return shallow(
+      <Modal />,
+      { context: { store } },
+    );
+  }
+
+  function sameTestToCantAdd() {
+    wrapper.find(Button).simulate('press');
+    expect(store.getActions()).toEqual([]);
+    expect(Alert.alert.calledOnce).toBe(true);
+  }
+
+  beforeEach(() => {
+    alert = sinon.spy(Alert, 'alert');
+    wrapper = createWrapper().dive();
+    store.clearActions();
+  });
+
+  afterEach(() => {
+    alert.restore();
+  });
+
+  it('Render 3 fields for user to fill', () => {
+    expect(wrapper.find(Input)).toHaveLength(2);
+    expect(wrapper.find(InputDatePicker)).toHaveLength(1);
+  });
+
+  it('Can\'t add event when anything is blank', () => {
+    sameTestToCantAdd();
+  });
+
+  it('Can\'t add event when only datetime is blank', () => {
+    wrapper.setState({
+      name,
+      where,
+    });
+    sameTestToCantAdd();
+  });
+
+  it('Can\'t add event when only name is blank', () => {
+    wrapper.setState({
+      datetime,
+      where,
+    });
+    sameTestToCantAdd();
+  });
+
+  it('Can\'t add event when only where is blank', () => {
+    wrapper.setState({
+      datetime,
+      name,
+    });
+    sameTestToCantAdd();
+  });
+});
