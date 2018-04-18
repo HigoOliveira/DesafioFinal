ed6b201c57aea60d9fb45722735e75f605cfcb7d
Higo Ribeiro <HORBJN@gmail.com>
2018-04-18 14:08:36 -0300

    DayCalendar já adiciona ou subtrai um dia

--- src/pages/Home/components/DayCalendar/index.js
+++ src/pages/Home/components/DayCalendar/index.js
@@ -1,18 +1,52 @@
-import React from 'react';
+/* Core */
+import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import moment from 'moment';
+
+/* Presentational */
+import { View, Text, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 import { colors } from 'styles';
 
 import styles from './styles';
 
-const DayCalendar = () => (
-  <View style={styles.container}>
-    <Icon name="caret-left" color={colors.white} size={16} style={styles.arrow} />
-    <Text style={styles.text}>Segunda, 8 de Fev</Text>
-    <Icon name="caret-right" color={colors.white} size={16} style={styles.arrow} />
-  </View>
-);
+class DayCalendar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      day: moment(),
+    };
+  }
+
+  pressRight = () => {
+    this.state.day.add(1, 'days');
+    this.forceUpdate();
+  }
+
+  pressLeft = () => {
+    this.state.day.subtract(1, 'days');
+    this.forceUpdate();
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={this.pressLeft}
+        >
+          <Icon name="caret-left" color={colors.white} size={16} style={styles.arrow} />
+        </TouchableOpacity>
+        <Text style={styles.text}>{this.state.day.format('dddd, D [de] MMM')}</Text>
+        <TouchableOpacity
+          onPress={this.pressRight}
+        >
+          <Icon name="caret-right" color={colors.white} size={16} style={styles.arrow} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 export default DayCalendar;
