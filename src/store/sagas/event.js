a07011cf08e67d3c5840b28d1b808fcad35aaf9b
Higo Ribeiro <HORBJN@gmail.com>
2018-05-07 15:17:32 -0300

    Adiciona evento com proprietário

--- src/store/sagas/event.js
+++ src/store/sagas/event.js
@@ -1,12 +1,17 @@
 import api from 'services/api';
-import { call, put } from 'redux-saga/effects';
+import { call, put, select } from 'redux-saga/effects';
 import ActionCreators from 'store/ducks/event';
 
 export function* addEvent(action) {
+  const { token } = yield select(state => state.user);
   const response = yield call(api.post, '/api/event/create', {
     name: action.name,
     where: action.where,
     datetime: action.datetime,
+  }, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
   });
   if (response.ok) {
     yield put(ActionCreators.eventAddNewSuccess(action.id, response.data.id));
