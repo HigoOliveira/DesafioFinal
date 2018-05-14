797b1dcda9d44117eec60cb2e4b262038ed35df2
Higo Ribeiro <HORBJN@gmail.com>
2018-05-14 10:47:06 -0300

    Desnecessário

--- src/services/auth.js
+++
@@ -1,8 +0,0 @@
-import { AsyncStorage } from 'react-native';
-
-export const TOKEN_KEY = '@DesafioFinal:token';
-
-export const isSignedIn = async () => {
-  const token = await AsyncStorage.getItem(TOKEN_KEY);
-  return (token !== null);
-};
